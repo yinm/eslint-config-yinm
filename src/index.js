f2a7f72d19466423dd38709f712927ed41c63fb9
yinm <yinm.cminor9@gmail.com>
2024-06-16 12:34:19 +0900

    Add stricter option for unused disable directives

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 /** @type {import('eslint').Linter.FlatConfig[]} */
 export default [
   {
+    linterOptions: {
+      reportUnusedDisableDirectives: "error",
+    },
     rules: {
       semi: [2, "always"],
     },
