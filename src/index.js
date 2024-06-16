4df29564d3c6f98fff100af77f4dd0f002c86a68
yinm <yinm.cminor9@gmail.com>
2024-06-16 19:26:02 +0900

    Add rules for `eslint-plugin-import-x`

--- src/index.js
+++ src/index.js
@@ -1,8 +1,36 @@
 import js from "@eslint/js";
+import pluginImport from "eslint-plugin-import-x";
 
 /** @type {import('eslint').Linter.FlatConfig[]} */
 export default [
   js.configs.recommended,
+
+  {
+    plugins: {
+      import: pluginImport,
+    },
+    rules: {
+      // Helpful warnings
+      "import/export": "error",
+      "import/no-deprecated": "error",
+      "import/no-extraneous-dependencies": "error",
+      "import/no-mutable-exports": "error",
+
+      // Static analysis
+      "import/no-absolute-path": "error",
+      "import/no-self-import": "error",
+
+      // Style guide
+      "import/first": "error",
+      "import/newline-after-import": "error",
+      "import/no-duplicates": "error",
+      "import/no-named-default": "error",
+      "import/order": [
+        "error",
+        { alphabetize: { order: "asc", orderImportKind: "asc" } },
+      ],
+    },
+  },
 
   {
     linterOptions: {
--- test/cases/index.js
+++ test/cases/index.js
@@ -1,5 +1,8 @@
 // prettier-ignore
-
 // if next disable directive is meaningless, error will be thrown by `reportUnusedDisableDirectives: "error"`
+// eslint-disable-next-line import/newline-after-import
+import foo from "./foo";
+foo.bar();
+
 // eslint-disable-next-line no-unused-vars
 const a = "b"
