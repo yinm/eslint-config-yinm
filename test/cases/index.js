4fb4cb5815243970320a9068e63b27a4e21e1ef6
yinm <yinm.cminor9@gmail.com>
2024-06-16 19:26:40 +0900

    Remove unnecessary comment

--- test/cases/index.js
+++ test/cases/index.js
@@ -1,8 +1,7 @@
-// prettier-ignore
 // if next disable directive is meaningless, error will be thrown by `reportUnusedDisableDirectives: "error"`
 // eslint-disable-next-line import/newline-after-import
 import foo from "./foo";
 foo.bar();
 
 // eslint-disable-next-line no-unused-vars
-const a = "b"
+const a = "b";
